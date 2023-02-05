bdde44d94828a1208c37702a13c19ed35b3a428a
Shraddhanand <1shraddhanand@gmail.com>
2023-02-05 17:31:21 +0530

    npm install -g expo-cli

--- index.js
+++
@@ -1,5 +0,0 @@
-// https://github.com/sahilpayla/My-Native-React.git
-
-// hello world
-
-// REACT NATIVE DAY 1 ---> Beginner Priject and Setup Today
---
+++ starting_files.js
@@ -0,0 +1,9 @@
+// https://github.com/sahilpayla/My-Native-React.git
+
+
+
+// REACT NATIVE DAY 1 ---> Beginner Project and Setup Today
+
+
+// npx create-react-app ./  --> REACT JS
+// npm install -g expo-cli  ---> REACT NATIVE
